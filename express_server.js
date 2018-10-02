89f0e653b8a13fb40997478704c2e74a24dc8e6c
ZoeElizabeth <zoe.gelech@gmail.com>
2018-10-02 21:37:04 +0000

    created index page displaying current short and long urls in database, starting the show page

--- express_server.js
+++ express_server.js
@@ -1,16 +1,37 @@
+//*************Tiny App Set Up*************
+
+//Dependancies
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+//Middleware
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+//Global Objects
+let urlDatabase = {
+  'b2xVn2': {shortURL:'b2xVn2', longURL: 'http://www.lighthouselabs.ca'},
+  '9sm5xK': {shortURL:'9sm5xK', longURL: 'http://www.google.com'},
+ };
+
+ //Listening on local host port
+ app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+
+//*************GET requests*************
+app.get("/", (req, res) => {
+  res.redirect("/urls")
+});
+
+//List all shortURL's and longURls's
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
 });
