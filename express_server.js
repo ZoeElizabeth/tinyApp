cfc62eaf10cf8713a39c23d5c64ed0efc004d200
ZoeElizabeth <zoe.gelech@gmail.com>
2018-10-02 23:41:24 +0000

    created form to make short urls, set up post that is receiving correct response, created function to create new shortURL keys

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 //*************Tiny App Set Up*************
 
 //Dependancies
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 //Middleware
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 //Global Objects
 let urlDatabase = {
@@ -14,16 +16,28 @@ let urlDatabase = {
   '9sm5xK': {shortURL:'9sm5xK', longURL: 'http://www.google.com'},
  };
 
-
  //Listening on local host port
  app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//*************Functions*************
+
+//Random String for ShortUrl Generator
+function generateRandomString() {
+  let key = '';
+  let charPool = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz'
+
+  for (let i = 0; i < 6 ; i++) {
+    key += charPool.charAt(Math.floor(Math.random() * charPool.length));
+  }
+      return key;
+};
 
 //*************GET requests*************
 app.get("/", (req, res) => {
-  res.redirect("/urls")
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 //List all shortURL's and longURls's
@@ -32,9 +46,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//Create new shortURL
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//Show all URLs in urlDatabase
 app.get("/urls/:id", (req, res) => {
   let templateVars = { 
     urls: urlDatabase,
     shortURL: req.params.id };
   res.render("urls_show", templateVars);
+});
+
+//*************POST requests*************
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok");         
 });
