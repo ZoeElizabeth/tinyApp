797f6f19ddc8752459b337bb91fc68992187a194
ZoeElizabeth <zoe.gelech@gmail.com>
2018-10-04 01:43:42 +0000

    user added to user object

--- express_server.js
+++ express_server.js
@@ -8,12 +8,15 @@ const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
 
 
-
-
 //Middleware
 app.set("view engine", "ejs");
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
+
+ //Listening on local host port
+ app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 //Global Objects
 let urlDatabase = {
@@ -21,10 +24,18 @@ let urlDatabase = {
   '9sm5xK': {shortURL:'9sm5xK', longURL: 'http://www.google.com'},
  };
 
- //Listening on local host port
- app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+ const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 //*************Functions*************
 
@@ -39,7 +50,83 @@ function generateShortURL() {
       return key;
 };
 
+
+//*************POST requests*************
+
+//cookie login
+app.post('/urls/login', (req, res)=> {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  res.cookie('email', req.body.email);
+  res.redirect('/urls');
+});
+
+//cookie logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('email', req.body.email);
+  res.redirect('/urls');
+});
+
+// Registration Requirments
+app.post('/urls/register', (req, res) => {
+
+  let email = req.body.email;
+  let password = req.body.password;
+
+   let genId = generateShortURL();
+    let newUser = {id: genId, email: email, password: password};
+      users[genId] = newUser
+  console.log(users)
+     
+res.redirect('/urls')
+});
+
+//Creating new shortURLs tied to longURLS 
+app.post("/urls", (req, res) => {
+  let makeShortURL = generateShortURL()
+  let currentShortURL = makeShortURL
+  let makeLongURL = req.body['longURL'] 
+
+  //add new urls to database
+  urlDatabase[currentShortURL] = {
+        shortURL: currentShortURL,
+        longURL: makeLongURL,
+      };
+  res.redirect('/urls/' + makeShortURL) 
+});
+
+// Delete existing URL
+app.post('/urls/:id/delete', (req, res) => {
+let targetId = req.params.id;
+  delete urlDatabase[targetId]
+
+res.redirect('/urls');
+});
+
+// Edit existing URL
+app.post('/urls/:id/edit', (req, res)=> {
+  let targetId = req.params.id;
+  let databaseKey = urlDatabase[targetId]
+
+  databaseKey.longURL = req.body['longURL']
+
+  res.redirect('/urls')
+});
+
 //*************GET requests*************
+//Create account/register page
+app.get('/urls/register', (req, res) => {  
+  let email = res.cookie.email
+  let templateVars = {
+    users: email,
+    urls: urlDatabase,
+  };
+  res.render("urls_register", templateVars);
+  });
+
+
+//redirect default
 app.get("/", (req, res) => {
 
   res.redirect("/urls");
@@ -84,55 +171,5 @@ app.get("/urls/:id", (req, res) => {
       username: req.cookies["email"] 
     };
   res.render("urls_show", templateVars);
-});
-
-
-//*************POST requests*************
-
-//cookie login
-app.post('/urls/login', (req, res)=> {
-  let email = req.body.email;
-  let password = req.body.password;
-
-  res.cookie('email', req.body.email);
-  res.redirect('/urls');
-});
-
-//cookie logout
-app.post('/logout', (req, res) => {
-  res.clearCookie('email', req.body.email);
-  res.redirect('/urls');
-});
-
-//Creating new shortURLs tied to longURLS 
-app.post("/urls", (req, res) => {
-  let makeShortURL = generateShortURL()
-  let currentShortURL = makeShortURL
-  let makeLongURL = req.body['longURL'] 
-
-  //add new urls to database
-  urlDatabase[currentShortURL] = {
-        shortURL: currentShortURL,
-        longURL: makeLongURL,
-      };
-  res.redirect('/urls/' + makeShortURL) 
-});
-
-// Delete existing URL
-app.post('/urls/:id/delete', (req, res) => {
-let targetId = req.params.id;
-  delete urlDatabase[targetId]
-
-res.redirect('/urls');
-});
-
-// Edit existing URL
-app.post('/urls/:id/edit', (req, res)=> {
-  let targetId = req.params.id;
-  let databaseKey = urlDatabase[targetId]
-
-  databaseKey.longURL = req.body['longURL']
-
-  res.redirect('/urls')
 });
 
